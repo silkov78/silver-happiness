fa7afc141f4bf274a667c4f5aa25957af82b2c8e
silkov78 <piotr.silkov78@gmail.com>
2025-01-03 16:30:09 +0300

    test_it_resolves_route

--- tests/Unit/RouterTest.php
+++ tests/Unit/RouterTest.php
@@ -85,4 +85,31 @@ class RouterTest extends TestCase
         $this->router->resolve($requestUri, $requestMethod);
     }
 
+    public function test_it_resolves_route_from_a_closure(): void
+    {
+        $this->router->get('/users', fn() => [1, 2 ,3]);
+
+        $this->assertEquals(
+            [1, 2, 3],
+            $this->router->resolve('/users', 'get')
+        );
+    }
+
+    public function test_it_resolves_route(): void
+    {
+        $users = new class() {
+            public function index(): array
+            {
+                return [1, 2, 3];
+            }
+        };
+
+        $this->router->get('/users', [$users::class, 'index']);
+
+        $this->assertEquals(
+            [1, 2, 3],
+            $this->router->resolve('/users', 'get')
+        );
+    }
+
 }
