2fc5152671be17f37d5cd9149068ecaf8be4b03d
silkov78 <piotr.silkov78@gmail.com>
2025-01-08 15:49:43 +0300

    it has sent the real email ...

--- tests/Unit/Services/InvoiceServiceTest.php
+++ tests/Unit/Services/InvoiceServiceTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Services;
 
+use App\Services\InvoiceService;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -13,5 +14,15 @@ use PHPUnit\Framework\TestCase;
 
 class InvoiceServiceTest extends TestCase
 {
+    public function test_it_processes_invoice(): void
+    {
+        // given InvoiceService 
+        $invoiceService = new InvoiceService();
 
+        // when process-method was called
+        $result = $invoiceService->process(['Piotr', 'chill guy'], 178);
+
+        // then assert process-method was executed (returned true)
+        $this->assertTrue($result);
+    }
 }
