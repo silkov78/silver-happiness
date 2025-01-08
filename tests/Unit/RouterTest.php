b0b70b6172070d91e319a2f0747786e3d187c37c
silkov78 <piotr.silkov78@gmail.com>
2025-01-08 10:38:26 +0300

    test exercise: test_there_are_no_routes_when_router_is_created and setUp method

--- tests/Unit/RouterTest.php
+++ tests/Unit/RouterTest.php
@@ -4,112 +4,39 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
 use App\Router;
 
 use App\Exceptions\RouteNotFoundException;
+use PHPUnit\Framework\TestCase;
+
+    /**
+     * ToDo
+     * 1. - test_there_are_no_routes_when_router_is_created
+     *    - create setUp with Router initiazlization
+     * 2. test_it_registers_a_route 
+     * 3. test_it_registers_a_post_route 
+     * 4. test_it_registers_a_get_route 
+     * 5. test_it_throws_route_not_found_exception 
+     * 6. test_it_resolves_route_from_a_closure 
+     *
+     */
 
 class RouterTest extends TestCase
 {
     private Router $router;
-
-    protected function setUp(): void
+    
+    public function setUp(): void
     {
         $this->router = new Router();
     }
 
     public function test_there_are_no_routes_when_router_is_created(): void
     {
-        $router = new Router();
-        $this->assertEmpty($router->routes());
+        // given object of Router $router
+        // when routes doesn't registered
+        $this->router = new Router();
+
+        // then $router->routes should return empty array
+        $this->assertEmpty($this->router->routes());
     }
-
-    public function test_it_registers_a_route(): void
-    {
-        $this->router->register('get', '/users', ['UserController', 'index']);
-
-        $expected = [
-            'get' => [
-                '/users' => ['UserController', 'index'],
-            ],
-        ];
-
-        $this->assertSame($expected, $this->router->routes());
-    }
-
-    public function test_it_registers_a_post_route(): void
-    {
-        $this->router->post('/users', ['UserController', 'store']);
-        $expected = [
-            'post' => [
-                '/users' => ['UserController', 'store'],
-            ],
-        ];
-
-        $this->assertSame($expected, $this->router->routes());
-    }
-
-    public function test_it_registers_a_get_route(): void
-    {
-        $this->router->get('/users', ['UserController', 'index']);
-
-        $expected = [
-            'get' => [
-                '/users' => ['UserController', 'index'],
-            ],
-        ];
-
-        $this->assertSame($expected, $this->router->routes());
-    }
-
-    /**
-    * @test
-    * @dataProvider \Tests\DataProviders\RouterDataProvider::routeNotFoundCases
-    */
-    public function test_it_throws_route_not_found_exception(
-        string $requestUri,
-        string $requestMethod
-    ): void
-    {
-        $users = new class() {
-            public function delete()
-            {
-                return true;
-            }
-        };
-
-        $this->router->post('/users', [$users::class, 'storage']);
-        $this->router->get('/users', [$users::class, 'index']);
-
-        $this->expectException(RouteNotFoundException::class);
-        $this->router->resolve($requestUri, $requestMethod);
-    }
-
-    public function test_it_resolves_route_from_a_closure(): void
-    {
-        $this->router->get('/users', fn() => [1, 2 ,3]);
-
-        $this->assertSame(
-            [1, 2, 3],
-            $this->router->resolve('/users', 'get')
-        );
-    }
-
-    public function test_it_resolves_route(): void
-    {
-        $users = new class() {
-            public function index(): array
-            {
-                return [1, 2, 3];
-            }
-        };
-
-        $this->router->get('/users', [$users::class, 'index']);
-
-        $this->assertSame(
-            [1, 2, 3],
-            $this->router->resolve('/users', 'get')
-        );
-    }
-
 }
