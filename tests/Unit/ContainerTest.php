3649c3d22d69690a0c4dc368ee3c0f901912e48e
silkov78 <piotr.silkov78@gmail.com>
2025-01-12 16:59:35 +0300

    setUp in  containerTEst

--- tests/Unit/ContainerTest.php
+++ tests/Unit/ContainerTest.php
@@ -11,82 +11,18 @@ use PHPUnit\Framework\TestCase;
 class ContainerTest extends TestCase
 {   
     private Container $container;
-    private $controller;
+    private $anonClass;
 
     public function setUp(): void
     {
         $this->container = new Container();
 
-        $this->controller = new class() {
-            public function charge()
+        $this->anonClass = new class() {
+            public function process()
             {
-                return 'Bobruisk';
+                return 'Hello!';
             }
         };
     }
 
-    public function test_has_method_for_unset_entry(): void
-    {   
-        // given container with empty entries and cotroller class
-        // when container->has(controller) was called
-        $result = $this->container->has($this->controller::class);
-
-        // then we assert has method returns false
-        $this->assertFalse($result);
-    }
-
-    public function test_set_method_with_callable(): void
-    {   
-        // given container with empty entries and cotroller class
-        // when container->set(controller)->charge was called
-        $this->container->set(
-            $this->controller::class,
-            fn(Container $c) => $this->controller::class
-        );
-
-        $result = $this->container->has($this->controller::class);
-
-        // then we assert result equal true
-        $this->assertTrue($result);
-    }
-
-    public function test_set_method_with_string(): void
-    {   
-        // given container with empty entries and cotroller class
-        // when container->set(controller)->charge was called
-        $this->container->set(
-            $this->controller::class,
-            $this->controller::class
-        );
-
-        $result = $this->container->has($this->controller::class);
-
-        // then we assert result equal true
-        $this->assertTrue($result);
-    }
-
-    public function test_get_method_for_unset_entry(): void
-    {   
-        // given container with empty entries and cotroller class
-        // when container->get(controller)->charge was called
-        $result = $this->container->get($this->controller::class)->charge();
-
-        // then we assert method will be executed
-        $this->assertSame('Bobruisk', $result);
-    }
-
-    public function test_get_method_for_set_entry(): void
-    {   
-        // given container with entries and cotroller class
-        $this->container->set(
-            $this->controller::class,
-            fn(Container $c) => $this->controller
-        );
-
-        // when container->get(controller)->charge was called
-        $result = $this->container->get($this->controller::class)->charge();
-
-        // then we assert method will be executed
-        $this->assertSame('Bobruisk', $result);
-    }
 }
