f9d3f346536800e558b52fab36d38b74e179e97b
silkov78 <piotr.silkov78@gmail.com>
2025-01-20 14:47:13 +0300

    RouteInterface

--- app/Attributes/Route.php
+++ app/Attributes/Route.php
@@ -5,9 +5,10 @@ declare(strict_types = 1);
 namespace App\Attributes;
 
 use Attribute;
+use App\Contracts\RouteInterface;
 
 #[Attribute]
-class Route
+class Route implements RouteInterface
 {
     public function __construct(
         public string $path, public string $method = 'get'
